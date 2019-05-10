a7618d9c79115a4fb63fd66eba380567113526f7
Matt Bonderer <mcbonderer10@gmail.com>
2019-05-10 15:17:38 -0500

    Initialized Project
    initialized project and finished pet.cs class

---
+++ Pets/Pets/Pet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Pet
+    {
+        public string type;
+        public string name;
+        public string owner;
+        public double weight;
+
+        public Pet(string Type, string Name, string Owner, double Weight)
+        {
+            type = Type;
+            name = Name;
+            owner = Owner;
+            weight = Weight;
+
+        }
+
+        public string getTag()
+        {
+            string tag = String.Format("If lost, call {0}.", owner);
+            return tag;
+        }
+    }
+}
---
+++ Pets/Pets/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet pet1 = new Pet("dog", "Shadow", "Jose", 42.6);
+
+            Console.WriteLine("Name: " + pet1.name);
+            Console.WriteLine("Weight: " + pet1.weight);
+            Console.WriteLine(pet1.getTag());
+
+            Console.ReadKey();
+        }
+    }
+}
